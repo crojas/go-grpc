632205a71ace4e0203e35ae8d478f5491ef352a1
unknown <c.rojas.latorre@gmail.com>
2019-08-19 17:59:00 -0400

    invocar funcion greet

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
@@ -17,5 +18,20 @@ func main() {
 	}
 	defer cc.Close()
 	c := greetpb.NewGreetServiceClient(cc)
-	fmt.Printf("Cliente creado %f", c)
+	//fmt.Printf("Cliente creado %f", c)
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Carlos",
+			LastName:  "Rojas",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error llamando a la funcion Greet del grpc %v", err)
+	}
+	log.Printf("Respuesta Greet GRPC: %v", res.Result)
 }
