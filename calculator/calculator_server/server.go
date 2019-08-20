f24fda6204fe2a177f02e72dac21b97d1bb1a936
unknown <c.rojas.latorre@gmail.com>
2019-08-20 18:13:23 -0400

    add divisor numero primo al server stream

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -13,12 +13,30 @@ import (
 type server struct{}
 
 func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
-	fmt.Printf("funcion sum invocada con req %v\n", req)
+	fmt.Printf("funcion Sum invocada con req %v\n", req)
 	res := req.GetFirstNumber() + req.GetSecondNumber()
 	result := &calculatorpb.SumResponse{
 		SumResult: res,
 	}
 	return result, nil
+}
+
+func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	fmt.Printf("funcion PrimeNumberDecomposition invocada con req %v\n", req)
+	number := req.GetNumber()
+	divisor := int64(2)
+	for number > 1 {
+		if number%divisor == 0 {
+			stream.Send(&calculatorpb.PrimeNumberDecompositionResponse{
+				PrimeFactor: divisor,
+			})
+			number = number / divisor
+		} else {
+			divisor++
+			fmt.Println("Aumentando el dividor a %v", divisor)
+		}
+	}
+	return nil
 }
 
 func main() {
